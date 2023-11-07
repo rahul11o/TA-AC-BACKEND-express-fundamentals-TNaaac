9d9d5e0c115c23bf57b11ee2fc7ee4b6d8f09e9e
rahul raushan <raushanrahul23@gmail.com>
2023-11-07 19:38:27 +0530

    third party middleware

---
+++ block-BNaabq/sample/server.js
@@ -0,0 +1,32 @@
+let express = require("express");
+let logger = require("morgan");
+let cookieParser = require("cookie-parser");
+
+let app = express();
+
+// app.use((req,res,next)=>{
+//     console.log(req.method, req.url)
+//     next()
+// })
+
+app.use(logger("dev"));
+app.use(cookieParser());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.cookie("username", "suraj");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(req.cookies);
+  next();
+});
+
+app.get("/about", (req, res, next) => {
+  res.send("Welcome!");
+});
+
+app.listen(5344, () => {
+  console.log("server listening on port 5344");
+});
